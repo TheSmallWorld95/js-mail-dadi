c4a71329e09a19154386b8199d0a66114e624700
Fabio-Colombo <thesmallworld95@gmail.com>
2021-03-02 18:15:42 +0100

    Completo con Bonus

---
+++ bonus/main.js
@@ -0,0 +1,25 @@
+// Faccio scegliere un numero da 1 a 6 all'utente 
+var numeroUtente = (Math.floor(Math.random() * (6 - 1)) + 1);
+
+
+// if (numeroUtente>=1 && numeroUtente <= 6) {
+//   console.log("Bravo");
+// } else {
+//   console.log("Non hai inserito un numero da 1 a 6");
+// }
+
+// Faccio scegliere un numero da 1 a 6 al computer
+var numeroComputer = (Math.floor(Math.random() * (6 - 1)) + 1);
+
+console.log(numeroUtente);
+console.log(numeroComputer);
+
+// Confronto i due numeri scelti e il più alto dei 2 vince
+
+if (numeroUtente > numeroComputer) {
+  console.log("Hai vinto");
+} else if (numeroUtente < numeroComputer) {
+  console.log("Hai perso");
+} else {
+  console.log("I due numeri sono uguali");
+}
